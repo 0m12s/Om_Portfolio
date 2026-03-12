b53282a03b577ef6710c3fc857717c3cd9a0df17
Om Sood <112969671+0m12s@users.noreply.github.com>
2026-03-12 06:08:47 +0530

    Delete src directory

--- src/main/java/com/om/portfolio/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.om.portfolio.repository;
-
-import com.om.portfolio.entity.ContactMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends JpaRepository<ContactMessage, Long> {
-}
